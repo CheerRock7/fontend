16763820c42cd14b4b57e4f1127d03107cd86d14
CheerRock7 <puti6305@gmail.com>
2024-08-16 20:21:42 +0700

    aa

--- src/app/component/navbar.js
+++ src/app/component/navbar.js
@@ -1,39 +1,40 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Navbar() {
     return (
-<>
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">สายการบินไปด้วยกันมั้ย</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-        <Link className="nav-link" href="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" href="/about">about</Link>
-        </li>
-        <li className="nav-item">
-        <Link className="nav-link" href="/service">service</Link>
-        </li>
-        <li className="nav-item">
-        <Link className="nav-link" href="/contact">Contact</Link>
-        </li>
-      </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
-
-</>
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">สายการบินไปด้วยกันมั้ย</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/about">About</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/service">Service</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                        <div className="d-flex ms-auto">
+                            <Link className="btn btn-outline-primary me-2" href="/signin">Sign In</Link>
+                            <Link className="btn btn-primary" href="/signup">Sign Up</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
     );
-  }
-  
+}
